fad1da076f4f9538be9604c1a608bb522f6059e3
Malva <ig.orozco@duocuc.cl>
2024-10-14 02:37:21 -0300

    a

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
+  registerForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      nombre: ['', [Validators.required, Validators.minLength(2)]],
+      apellido: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, this.matchPasswords('password')]], // Validación personalizada
+      birthdate: ['', Validators.required]
+    });
   }
 
+  // Validación personalizada para confirmar que las contraseñas coincidan
+  matchPasswords(password: string) {
+    return (control: any) => {
+      const formGroup = control.parent;
+      if (formGroup) {
+        const passwordControl = formGroup.controls[password];
+        if (passwordControl && passwordControl.value !== control.value) {
+          return { passwordsDoNotMatch: true };
+        }
+      }
+      return null;
+    };
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Formulario válido:', this.registerForm.value);
+      // Lógica adicional para enviar los datos del formulario al servidor
+    } else {
+      console.log('Formulario no válido');
+    }
+  }
+
+  // Método para acceder a los controles del formulario desde el HTML
+  get formControls() {
+    return this.registerForm.controls;
+  }
 }
