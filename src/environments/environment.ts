507260ed675a1d810e68fdbcca26777c7a92055a
Malva <ig.orozco@duocuc.cl>
2024-10-27 23:11:11 -0300

    asd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig : {
+  apiKey: "AIzaSyA8Uv8WhLTKxtLrJ7VJsHRB-gOiMedP2JU",
+  authDomain: "safecity-3d0be.firebaseapp.com",
+  projectId: "safecity-3d0be",
+  storageBucket: "safecity-3d0be.appspot.com",
+  messagingSenderId: "576190042556",
+  appId: "1:576190042556:web:17388af42f5d5feb4b9bb9",
+  measurementId: "G-6RQSC3RHLH"
+}
 };
 
 /*
